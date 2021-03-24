add7e724997ec5717a787d0b628c9fc5c1d4a2ea
huyudonglove <379145942@qq.com>
2021-03-24 03:02:55 +0000

    md5

--- upload.vue
+++ upload.vue
@@ -2,12 +2,12 @@
     <div>
     <el-upload
       drag
-      action
+      action=''
       :auto-upload="false" 
       :show-file-list="false" 
       :before-upload="beforeUp"
       :on-change='change'
-      
+      :limit='1'
       >
       <i class="el-icon-upload"></i>
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传1111</em></div>
@@ -27,19 +27,21 @@ export default {
             allSlice:[],
             size:1024*1024,
             fileCopy:'',
-            indexC:''
+            indexC:'',
+            fileName:'',
+            str:""
         }
     },
     methods:{
         beforeUp(file){
             console.log(file);
            
-            if(file.size<=(1024*1024)){
+            if(file.size<=(this.size)){
                 console.log(file);
                 //axios.post('/api/')
                 return true
             }else{
-               this.allChuck=Math.ceil(file.size/(1024*1024));
+               this.allChuck=Math.ceil(file.size/(this.size));
                console.log(this.allChuck,7777)
                this.checkMd5(file);
             }
@@ -47,33 +49,42 @@ export default {
         change(file) {
              this.allSlice=[]; 
              this.fileCopy=file.raw;
+             this.fileName=file.raw.name;
+             console.log(this.fileName,777)
              this.beforeUp(this.fileCopy)
         },
-      async checkMd5(file){
+      checkMd5(file){
           var self=this;
-          const fileRead = new FileReader();
-          const sparkMD5 = new SparkMD5.ArrayBuffer()
-          await fileRead.readAsArrayBuffer(file);
-          await fileRead.addEventListener('load',function(e){
-              console.log(e);
-              self.bufferFile=e.target.result;
-              for(let i=0;i<self.allChuck;i++){
-                console.log(i,89);
-                
-               self.allSlice.push(new Blob([self.bufferFile.slice(i*self.size,(i+1)*self.size)]));
-               
-              }
-              console.log(self.allSlice,9999)  
-          });
-          
-          
-          sparkMD5.append(this.bufferFile);
-          this.md5= sparkMD5.end();
-          console.log(this.md5);
-          await this.checkUp(this.md5);
+          this.md5='';
+          const sparkMD5 = new SparkMD5();
+         for(let i=0;i<self.allChuck;i++){
+            let park=file.slice(i*self.size,(i+1)*self.size);
+            self.allSlice.push(park);
+            var reader = new FileReader();
+            if(i==0){
+                reader.readAsText(park)
+                reader.onload = function(event) {
+                   self.str=event.target.result.slice(0,1)+self.str   
+             };
+            }
+            if(i==(self.allChuck*1-1)){
+                reader.readAsText(park);
+                reader.onload = function(event) {
+                   self.str=event.target.result.slice(0,1)+self.str;
+                   sparkMD5.append(self.str); 
+                    self.md5= sparkMD5.end();
+                    self.checkUp(self.md5);   
+             };
+                    
+            }
+            
+        }
+          console.log(self.allSlice,9999)  
       },
       //上传md5
       checkUp(md5){
+          console.log(this.str,'str')
+          console.log(md5,'md5');
           var self=this;
           axios.post('/api/file/fragment/checkFile',{fileMd5:md5}).then(res=>{
               console.log(res);
@@ -96,7 +107,7 @@ export default {
                    fileMd5:self.md5,
                    fragmentNum:this.allSlice.length,
                    currIndex:index,
-                   size:self.fileCopy.size
+                   size:self.size
                }
                formData.append('file',this.allSlice[current]);
                formData.append('fragmentDto',JSON.stringify(fragmentDto));
@@ -109,7 +120,11 @@ export default {
                return
            }
 
-       } 
+       },
+      
+    },
+    created(){
+        
     }
 }
 </script>
